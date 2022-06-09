5c19a6405ab6420308c9c59245d6ec31849c7b81
jazzrail <jazzrail@hotmail.com>
2022-06-09 16:49:14 +0100

    Redesigned Nav and links

--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,6 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+const menuItem = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
 
-  //   Toggle Nav
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-
-    // Animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.3
-        }s`;
-      }
-    });
-
-    // Burger Animation
-    burger.classList.toggle('toggle');
-  });
-};
-
-navSlide();
+menuItem.addEventListener('click', () => {
+	navbar.classList.toggle('change');
+});
