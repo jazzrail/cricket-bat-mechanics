e7e0125f6a894df8e42712bfa775225b6a9be6d0
jazzrail <jazzrail@hotmail.com>
2022-08-22 18:34:28 +0100

    added 2rem margin to instructions button

--- js/calc.js
+++ js/calc.js
@@ -1,30 +1,30 @@
-document.getElementById("btn-calc").addEventListener("click", (e) => {
+document.getElementById('btn-calc').addEventListener('click', (e) => {
   e.preventDefault();
 
-  const handleLen = parseFloat(document.querySelector("#handleLen").value);
-  if (document.getElementById("handleLen").value.length == 0) {
-    alert("Please enter integer or decimal in each of the fields");
-    document.classList.remove("empty-field");
+  const handleLen = parseFloat(document.querySelector('#handleLen').value);
+  if (document.getElementById('handleLen').value.length == 0) {
+    alert('Please enter integer or decimal in each of the fields');
+    document.classList.remove('empty-field');
   }
 
-  const bladeLen = parseFloat(document.querySelector("#bladeLen").value);
-  if (document.getElementById("bladeLen").value.length == 0) {
-    alert("Please enter integer or decimal in each of the fields");
-    document.classList.remove("empty-field");
+  const bladeLen = parseFloat(document.querySelector('#bladeLen').value);
+  if (document.getElementById('bladeLen').value.length == 0) {
+    alert('Please enter integer or decimal in each of the fields');
+    document.classList.remove('empty-field');
   }
-  const batMass = parseFloat(document.querySelector("#batMass").value);
-  if (document.getElementById("batMass").value.length == 0) {
-    alert("Please enter integer or decimal in each of the fields");
-    document.classList.remove("empty-field");
-  }
-
-  const balPoint = parseFloat(document.querySelector("#balPoint").value);
-  if (document.getElementById("balPoint").value.length == 0) {
-    alert("Please enter integer or decimal in each of the fields");
-    document.classList.remove("empty-field");
+  const batMass = parseFloat(document.querySelector('#batMass').value);
+  if (document.getElementById('batMass').value.length == 0) {
+    alert('Please enter integer or decimal in each of the fields');
+    document.classList.remove('empty-field');
   }
 
-  const res = document.getElementById("res");
+  const balPoint = parseFloat(document.querySelector('#balPoint').value);
+  if (document.getElementById('balPoint').value.length == 0) {
+    alert('Please enter integer or decimal in each of the fields');
+    document.classList.remove('empty-field');
+  }
+
+  const res = document.getElementById('res');
 
   // 1
   const bladeMass =
